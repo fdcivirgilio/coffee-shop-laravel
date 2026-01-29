002ca0ed1ae2ad721b6b63996be1b5f5e71a64fe
fdcivirgilio <fdc.virgilio@gmail.com>
2026-01-29 14:40:14 +0800

    fix login sign up error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
 
